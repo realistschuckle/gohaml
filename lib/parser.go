54a7f165796b8ce1413e29d12f83858623da0325
Curtis Schlak <realistschuckle@gmail.com>
2010-08-18 19:11:08 -0500

    About half-way toward finishing the rewrite.

--- lib/parser.go
+++ lib/parser.go
@@ -2,6 +2,7 @@ package gohaml
 
 import (
 	"unicode"
+	"strings"
 )
 
 type hamlParser struct {
@@ -9,20 +10,30 @@ type hamlParser struct {
 
 func (self *hamlParser) parse(input string) (output *tree) {
 	output = newTree()
-	node := parseLeadingSpace(input)
-	output.nodes.Push(node)
-		
+	var currentNode *node
+	j := 0
+	for i, r := range input {
+		if r == '\n' {
+			node := parseLeadingSpace(input[j:i])
+			if currentNode == nil  || node.indentLevel == currentNode.indentLevel {
+				output.nodes.Push(node)
+			} else if node.indentLevel > currentNode.indentLevel {
+				currentNode.children.Push(node)
+			}
+			currentNode = node
+			j = i + 1
+		}
+	}
+	node := parseLeadingSpace(input[j:])
+	if currentNode == nil  || node.indentLevel == currentNode.indentLevel {
+		output.nodes.Push(node)
+	} else if node.indentLevel > currentNode.indentLevel {
+		currentNode.children.Push(node)
+	}
 	return
 }
 
-func (self *hamlParser) initialize() {
-
-}
-
 var parser hamlParser
-
-func init() {
-}
 
 func parseLeadingSpace(input string) (output *node) {
 	node := new(node)
@@ -30,13 +41,34 @@ func parseLeadingSpace(input string) (output *node) {
 		switch {
 		case r == '%':
 			output = parseTag(input[i + 1:], node)
+		case r == '#':
+			output = parseId(input[i + 1:], node)
 		case r == '.':
 			output = parseClass(input[i + 1:], node)
+		case r == '=':
+			output = parseKey(tl(input[i + 1:]), node)
+		case r == '\\':
+			output = parseRemainder(input[i + 1:], node)
 		case !unicode.IsSpace(r):
 			output = parseRemainder(input[i:], node)
 		}
-		if nil != output {break}
+		if nil != output {
+			output.indentLevel = i
+			break
+		}
 	}
+	return
+}
+
+func parseKey(input string, node *node) (output *node) {
+	if input[len(input) - 1] == '<' {
+		output = parseNoNewline("", node)
+		output.remainder.value = input[0:len(input) - 1]
+	} else {
+		output = node
+		output.remainder.value = input
+	}
+	output.remainder.needsResolution = true
 	return
 }
 
@@ -45,6 +77,10 @@ func parseTag(input string, node *node) (output *node) {
 		switch {
 		case r == '.':
 			output = parseClass(input[i + 1:], node)
+		case r == '#':
+			output = parseId(input[i + 1:], node)
+		case r == '{':
+			output = parseAttributes(tl(input[i + 1:]), node)
 		case unicode.IsSpace(r):
 			output = parseRemainder(input[i + 1:], node)
 		}
@@ -60,30 +96,99 @@ func parseTag(input string, node *node) (output *node) {
 	return
 }
 
+func parseAttributes(input string, node *node) (output *node) {
+	inKey := true
+	inRocket := false
+	keyEnd, attrStart := 0, 0
+	for i, r := range input {
+		if inKey && (r == '=' || unicode.IsSpace(r)) {
+			inKey = false
+			inRocket = true
+			keyEnd = i
+		} else if inRocket && r != '>' && r != '=' && !unicode.IsSpace(r) {
+			inRocket = false
+			attrStart = i
+		} else if r == ',' {
+			node.addAttr(t(input[0:keyEnd]), t(input[attrStart:i]))
+			output = parseAttributes(tl(input[i + 1:]), node)
+			break
+		} else if r == '}' {
+			node.addAttr(input[0:keyEnd], t(input[attrStart:i]))
+			output = node
+			break
+		}
+	}
+	return
+}
+
+func parseId(input string, node *node) (output *node) {
+	for i, r := range input {
+		switch{
+		case r == '.':
+			node.addAttrNoLookup("id", input[0:i])
+			output = parseClass(input[i + 1:], node)
+		case r == '=':
+			node.addAttrNoLookup("id", input[0:i])
+			output = parseKey(tl(input[i + 1:]), node)
+		case unicode.IsSpace(r):
+			node.addAttrNoLookup("id", input[0:i])
+			output = parseRemainder(input[i + 1:], node)
+		}
+		if nil != output {break}
+	}
+	if nil == output {
+		output = node
+		node.addAttrNoLookup("id", input)
+	}
+	return
+}
+
 func parseClass(input string, node *node) (output *node) {
 	for i, r := range input {
 		switch {
 		case r == '.':
-			node.attrs.Push(&resPair{res{"class", false}, res{input[0:i], false}})
+			node.addAttrNoLookup("class", input[0:i])
 			output = parseClass(input[i + 1:], node)
+		case r == '=':
+			node.addAttrNoLookup("class", input[0:i])
+			output = parseKey(tl(input[i + 1:]), node)
 		case unicode.IsSpace(r):
-			node.attrs.Push(&resPair{res{"class", false}, res{input[0:i], false}})
+			node.addAttrNoLookup("class", input[0:i])
 			output = parseRemainder(input[i + 1:], node)
 		}
-		if nil != output {
-			break;
-		}
+		if nil != output {break}
 	}
 	if nil == output {
 		output = node
-		node.attrs.Push(&resPair{res{"class", false}, res{input, false}})
+		output.addAttrNoLookup("class", input)
 	}
 	return
 }
 
 func parseRemainder(input string, node *node) (output *node) {
-	output = node
-	output.remainder.value = input
+	if input[len(input) - 1] == '<' {
+		output = parseNoNewline("", node)
+		output.remainder.value = input[0:len(input) - 1]
+	} else {
+		output = node
+		output.remainder.value = input
+	}
 	output.remainder.needsResolution = false
+	return
+}
+
+func parseNoNewline(input string, node *node) (output *node) {
+	node.noNewline = true
+	output = node
+	return
+}
+
+func t(input string) (output string) {
+	output = strings.Trim(input, " 	")
+	return
+}
+
+func tl(input string) (output string) {
+ 	output = strings.TrimLeft(input, " 	")
 	return
 }
--- lib/tree.go
+++ lib/tree.go
@@ -1,6 +1,7 @@
 package gohaml
 
 import (
+	"bytes"
 	"container/vector"
 	"fmt"
 )
@@ -19,6 +20,9 @@ type node struct {
 	remainder res
 	name string
 	attrs vector.Vector
+	noNewline bool
+	indentLevel int
+	children vector.Vector
 }
 
 type tree struct {
@@ -32,36 +36,92 @@ func newTree() (output *tree) {
 
 func (self res) resolve(scope map[string]interface{}) (output string) {
 	output = self.value
+	if self.needsResolution {output = fmt.Sprint(scope[self.value])}
 	return
 }
 
 func (self tree) resolve(scope map[string]interface{}) (output string) {
-	for _, n := range self.nodes {
+	treeLen := self.nodes.Len()
+	buf := bytes.NewBuffer(make([]byte, 0))
+	for i, n := range self.nodes {
 		node := n.(*node)
-		output += node.resolve(scope)
+		node.resolve(scope, buf)
+		if i != treeLen - 1 && !node.noNewline {
+			buf.WriteString("\n")
+		}
 	}
+	output = buf.String()
 	return
 }
 
-func (self node) resolve(scope map[string]interface{}) (output string) {
+func (self *node) addAttr(key string, value string) {
+	keyLookup, valueLookup := true, true
+	if key[0] == ':' {
+		keyLookup = false
+		key = key[1:]
+	}
+	if value[0] == '"' {
+		valueLookup = false
+		value = value[1:len(value) - 1]
+	}
+	self.attrs.Push(&resPair{res{key, keyLookup}, res{value, valueLookup}})
+}
+
+func (self *node) addAttrNoLookup(key string, value string) {
+	self.attrs.Push(&resPair{res{key, false}, res{value, false}})
+}
+
+func (self node) resolve(scope map[string]interface{}, buf *bytes.Buffer) {
 	remainder := self.remainder.resolve(scope)
 	if self.attrs.Len() > 0 && len(remainder) > 0 {
 		if len(self.name) == 0 {self.name = "div"}
-		output = fmt.Sprintf("<%s%s>%s</%s>", self.name, self.resolveAttrs(scope), remainder, self.name)
+		buf.WriteString("<")
+		buf.WriteString(self.name)
+		self.resolveAttrs(scope, buf)
+		buf.WriteString(">")
+		buf.WriteString(remainder)
+		buf.WriteString("</")
+		buf.WriteString(self.name)
+		buf.WriteString(">")
 	} else if self.attrs.Len() > 0 {
 		if len(self.name) == 0 {self.name = "div"}
-		output = fmt.Sprintf("<%s%s />", self.name, self.resolveAttrs(scope))
+		buf.WriteString("<")
+		buf.WriteString(self.name)
+		self.resolveAttrs(scope, buf)
+		childLen := self.children.Len()
+		if childLen > 0 {
+			buf.WriteString(">")
+			for i, n := range self.children {
+				node := n.(*node)
+				node.resolve(scope, buf)
+				if i != childLen - 1 && !node.noNewline {
+					buf.WriteString("\n")
+				}
+			}
+			buf.WriteString("</")
+			buf.WriteString(self.name)
+			buf.WriteString(">")
+		} else {
+			buf.WriteString(" />")
+		}
 	} else if len(self.name) > 0 && len(remainder) > 0 {
-		output = fmt.Sprintf("<%s>%s</%s>", self.name, remainder, self.name)
+		buf.WriteString("<")
+		buf.WriteString(self.name)
+		buf.WriteString(">")
+		buf.WriteString(remainder)
+		buf.WriteString("</")
+		buf.WriteString(self.name)
+		buf.WriteString(">")
 	} else if len(self.name) > 0 {
-		output = fmt.Sprintf("<%s />", self.name)
+		buf.WriteString("<")
+		buf.WriteString(self.name)
+		buf.WriteString(" />")
 	} else {
-		output = remainder
+		buf.WriteString(remainder)
 	}
-	return
 }
 
-func (self node) resolveAttrs(scope map[string]interface{}) (output string) {
+func (self node) resolveAttrs(scope map[string]interface{}, buf *bytes.Buffer) {
 	attrMap := make(map[string]string)
 	for i := 0; i < self.attrs.Len(); i++ {
 		resPair := self.attrs.At(i).(*resPair)
@@ -73,7 +133,10 @@ func (self node) resolveAttrs(scope map[string]interface{}) (output string) {
 		}
 	}
 	for key, value := range attrMap {
-		output += " " + key + "=\"" + value + "\""
+		buf.WriteString(" ")
+		buf.WriteString(key)
+		buf.WriteString("=\"")
+		buf.WriteString(value)
+		buf.WriteString("\"")
 	}
-	return
 }
