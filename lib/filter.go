478f017a645a0b815a8073b9e522933ec0cbcc8c
Bryan Matsuo <bryan.matsuo@gmail.com>
2011-11-21 23:12:43 -0800

    Clean up the filter code a lot. All tests still pass.

--- lib/filter.go
+++ lib/filter.go
@@ -38,21 +38,28 @@ type FilterFunc func(content, indent string) string
 func (fn FilterFunc) Filter(content, indent string) string { return fn(content, indent) }
 
 func cdataHelper(pre, post, content, indent string) string {
-	if content == "" {
-		return fmt.Sprintf("%s<![CDATA[%s\n%s%s]]>%s",pre, post, indent, pre, post)
+	var tail string
+	if len(content) > 0 {
+		tail = "\n"
 	}
-	return fmt.Sprintf("%s<![CDATA[%s\n%s\n%s%s]]>%s", pre, post, content, indent, pre, post)
+	return fmt.Sprintf("%s<![CDATA[%s\n%s%s%s%s]]>%s", pre, post, content, tail, indent, pre, post)
 }
+
+func deepenIndent(str string) string {
+	if len(str) == 0 {
+		return ""
+	}
+	return "\t" + strings.Replace(str, "\n\t", "\n\t\t", -1)
+}
+
 func cdata(content, indent string) string { return cdataHelper("", "", content, indent) }
 func css(content, indent string) string {
-	if content == "" {
-		return fmt.Sprintf("<style type=\"text/css\">\n%s%s\n%s</style>", indent+"\t", cdataHelper("/*", "*/", content, indent+"\t"),indent)
-	}
-	return fmt.Sprintf("<style type=\"text/css\">\n%s%s\n%s</style>", indent+"\t", cdataHelper("/*", "*/", "\t" + strings.Replace(content, "\n\t", "\n\t\t", -1), indent+"\t"), indent)
+	nextindent := indent + "\t"
+	return fmt.Sprintf("<style type=\"text/css\">\n%s%s\n%s</style>",
+		nextindent, cdataHelper("/*", "*/", deepenIndent(content), nextindent), indent)
 }
 func javascript(content, indent string) string {
-	if content == "" {
-		return fmt.Sprintf("<script type=\"text/javascript\">\n%s%s\n%s</script>", indent+"\t", cdataHelper("//", "", content, indent+"\t"),indent)
-	}
-	return fmt.Sprintf("<script type=\"text/javascript\">\n%s%s\n%s</script>", indent+"\t", cdataHelper("//", "", "\t" + strings.Replace(content, "\n\t", "\n\t\t", -1), indent+"\t"), indent)
+	nextindent := indent + "\t"
+	return fmt.Sprintf("<script type=\"text/javascript\">\n%s%s\n%s</script>",
+		nextindent, cdataHelper("//", "", deepenIndent(content), nextindent), indent)
 }
