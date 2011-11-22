1ce745292963d55310e80d6f8e59899a98ea30f2
Bryan Matsuo <bryan.matsuo@gmail.com>
2011-11-21 23:03:55 -0800

    Add tests for nested filter blocks. Fix a few related indentation bugs.
    The first like of filter output is going to be indented automatically.
    So the filter functions have had their initial indentation removed, and
    a deeper indentation added before nested filter function calls.

--- lib/filter.go
+++ lib/filter.go
@@ -39,20 +39,20 @@ func (fn FilterFunc) Filter(content, indent string) string { return fn(content, 
 
 func cdataHelper(pre, post, content, indent string) string {
 	if content == "" {
-		return fmt.Sprintf("%s%s<![CDATA[%s\n%s%s]]>%s",indent, pre, post, indent, pre, post)
+		return fmt.Sprintf("%s<![CDATA[%s\n%s%s]]>%s",pre, post, indent, pre, post)
 	}
-	return fmt.Sprintf("%s%s<![CDATA[%s\n%s\n%s%s]]>%s", indent, pre, post, content, indent, pre, post)
+	return fmt.Sprintf("%s<![CDATA[%s\n%s\n%s%s]]>%s", pre, post, content, indent, pre, post)
 }
 func cdata(content, indent string) string { return cdataHelper("", "", content, indent) }
 func css(content, indent string) string {
 	if content == "" {
-		return fmt.Sprintf("%s<style type=\"text/css\">\n%s\n%s</style>",indent, cdataHelper("/*", "*/", content, indent+"\t"),indent)
+		return fmt.Sprintf("<style type=\"text/css\">\n%s%s\n%s</style>", indent+"\t", cdataHelper("/*", "*/", content, indent+"\t"),indent)
 	}
-	return fmt.Sprintf("%s<style type=\"text/css\">\n%s\n%s</style>", indent, cdataHelper("/*", "*/", "\t" + strings.Replace(content, "\n\t", "\n\t\t", -1), indent+"\t"), indent)
+	return fmt.Sprintf("<style type=\"text/css\">\n%s%s\n%s</style>", indent+"\t", cdataHelper("/*", "*/", "\t" + strings.Replace(content, "\n\t", "\n\t\t", -1), indent+"\t"), indent)
 }
 func javascript(content, indent string) string {
 	if content == "" {
-		return fmt.Sprintf("%s<script type=\"text/javascript\">\n%s\n%s</script>",indent, cdataHelper("//", "", content, indent+"\t"),indent)
+		return fmt.Sprintf("<script type=\"text/javascript\">\n%s%s\n%s</script>", indent+"\t", cdataHelper("//", "", content, indent+"\t"),indent)
 	}
-	return fmt.Sprintf("%s<script type=\"text/javascript\">\n%s\n%s</script>", indent, cdataHelper("//", "", "\t" + strings.Replace(content, "\n\t", "\n\t\t", -1), indent+"\t"), indent)
+	return fmt.Sprintf("<script type=\"text/javascript\">\n%s%s\n%s</script>", indent+"\t", cdataHelper("//", "", "\t" + strings.Replace(content, "\n\t", "\n\t\t", -1), indent+"\t"), indent)
 }
