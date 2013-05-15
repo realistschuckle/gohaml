5346e4593e749484fb681575f57ba08abee1e6c9
Tim Becker <tim.becker@kuriositaet.de>
2013-05-15 22:04:43 +0200

    saner renaming

--- haml_simple_test.go
+++ haml_simple_test.go
@@ -4,11 +4,14 @@ import (
   "testing"
 )
 
-const simple_haml = "%p(a='b')"
+//const simple_haml = "%p(a='b')"
+//const simple_haml = "%p{a=>'b'}"
+//const simple_haml = "%p{:a=>'b'}"
+const simple_haml = "%p{:a=>\"b\"}"
 const simple_html = "<p a='b'></p>"
 
 func TestSimple(t * testing.T) {
   engine, _ := NewEngine(simple_haml)
-  html := engine.Render(scope)
+  html := engine.Render(defaultScope)
   print(html)
 }
