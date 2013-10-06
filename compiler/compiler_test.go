d6cf7959bffd3b1e59cc8c845a12b3615e9fe032
Curtis Schlak <fuzzygeek1@users.sourceforge.net>
2013-10-06 11:09:20 -0500

    Make happy with the 1.1 decl.

--- compiler/compiler_test.go
+++ compiler/compiler_test.go
@@ -41,3 +41,21 @@ func TestDoctypeEmptySpecWithXhtmlFormat(t *testing.T) {
 	output := cdoc.Outputs[0].(*StaticOutput)
 	assert.Equal(t, output.Content, "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">")
 }
+
+func TestDoctype1Point1SpecWithXhtmlFormat(t *testing.T) {
+	opts := CompilerOpts{}
+	opts.Format = "xhtml"
+	nodes := []p.Node{&p.DoctypeNode{"1.1"}}
+	pdoc := p.ParsedDoc{}
+	pdoc.Nodes = nodes
+	compiler := DefaultCompiler{}
+
+	cdoc, e := compiler.Compile(pdoc, opts)
+
+	assert.Nil(t, e)
+	assert.NotNil(t, cdoc)
+	assert.Equal(t, len(cdoc.Outputs), 1)
+
+	output := cdoc.Outputs[0].(*StaticOutput)
+	assert.Equal(t, output.Content, "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.1//EN\" \"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd\">")
+}
