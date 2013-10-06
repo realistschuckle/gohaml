35713699ba04c46bbc6002079d7068552dbd0769
Curtis Schlak <fuzzygeek1@users.sourceforge.net>
2013-10-06 11:12:00 -0500

    Make happy with the mobile doctype declaration.

--- compiler/compiledoctype_test.go
+++ compiler/compiledoctype_test.go
@@ -59,3 +59,21 @@ func TestDoctype1Point1SpecWithXhtmlFormat(t *testing.T) {
 	output := cdoc.Outputs[0].(*StaticOutput)
 	assert.Equal(t, output.Content, "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.1//EN\" \"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd\">")
 }
+
+func TestDoctypeMobileSpecWithXhtmlFormat(t *testing.T) {
+	opts := CompilerOpts{}
+	opts.Format = "xhtml"
+	nodes := []p.Node{&p.DoctypeNode{"mobile"}}
+	pdoc := p.ParsedDoc{}
+	pdoc.Nodes = nodes
+	compiler := DefaultCompiler{}
+
+	cdoc, e := compiler.Compile(pdoc, opts)
+
+	assert.Nil(t, e)
+	assert.NotNil(t, cdoc)
+	assert.Equal(t, len(cdoc.Outputs), 1)
+
+	output := cdoc.Outputs[0].(*StaticOutput)
+	assert.Equal(t, output.Content, "<!DOCTYPE html PUBLIC \"-//WAPFORUM//DTD XHTML Mobile 1.2//EN\" \"http://www.openmobilealliance.org/tech/DTD/xhtml-mobile12.dtd\">")
+}
