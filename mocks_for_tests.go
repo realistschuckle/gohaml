f26945a64548b61009bb0cec2c744ae7eb1a2cf0
Curtis Schlak <realistschuckle@gmail.com>
2014-12-14 09:59:55 -0600

    Move and rename files from sub packages.

--- mocks_for_tests.go
+++ mocks_for_tests.go
@@ -1,8 +1,17 @@
-package parser
+package gohaml
 
 import (
 	"github.com/stretchr/testify/mock"
 )
+
+type mockCompiledOutput struct {
+	mock.Mock
+}
+
+func (self *mockCompiledOutput) Render(scope map[string]interface{}) (string, error) {
+	args := self.Mock.Called(scope)
+	return args.String(0), args.Error(1)
+}
 
 type mockNode struct {
 	mock.Mock
